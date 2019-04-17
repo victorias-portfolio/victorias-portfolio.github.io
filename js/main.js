ed1821052d2e716bd79d05dafb21405121c48a49
vicmacarmstrong <victoria.mac.armstrong@gmail.com>
2019-04-17 16:05:01 -0400

    made the dogs move

--- js/main.js
+++ js/main.js
@@ -2,10 +2,23 @@ var elAboutLink = document.getElementById('aboutLink');
 var elAbout = document.getElementById('about');
 var elCloseBtn = document.getElementById('closeBtn');
 
+var container = document.getElementById('container');
+var windowHeight = window.innerHeight;
+var windowWidth = window.innerWidth;
+var scrollArea = 1850 - windowHeight;
+var emoji = document.getElementsByClassName('emoji')[0];
+
 elAboutLink.addEventListener('click', function(e){
   elAbout.classList.toggle('showabout');
 });
 
 elCloseBtn.addEventListener('click', function(e){
   elAbout.classList.toggle('showabout');
+});
+
+window.addEventListener('scroll', function(e) {
+  var scrollTop = document.documentElement.scrollTop;
+  var scrollPercent = scrollTop/scrollArea || 0;
+
+  emoji.style.left = scrollPercent*window.innerWidth + 'px';
 });
